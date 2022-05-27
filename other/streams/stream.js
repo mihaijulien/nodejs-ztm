304a3b0110eae8b2f6b1e866b7d4eb6dcbd2a69a
Julien Mihai <mihaijulien@gmail.com>
2022-05-27 15:49:50 +0300

    fix path

--- other/streams/stream.js
+++ other/streams/stream.js
@@ -1,11 +1,12 @@
 var fs = require('fs');
 var http =  require('http');
-var file = './powder-day.mp4';
+var file = 'powder-day.mp4';
+var path = require('path');
 
 http.createServer((req, res) => {
 
     res.writeHeader(200, { 'Content-Type': 'video/mp4' });
-    fs.createReadStream(file)
+    fs.createReadStream(__dirname  + path.sep + file)
         .pipe(res)
         .on('error', console.error);
 
