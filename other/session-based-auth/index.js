be987cbfc25442114365f16078af03ed33923382
Julien Mihai <mihaijulien@gmail.com>
2022-08-08 23:44:09 +0300

    add login endpoint & store data inside session

--- other/session-based-auth/index.js
+++ other/session-based-auth/index.js
@@ -12,10 +12,14 @@ const RedisStore = connectRedis(session);
 
 // configure redis
 const redisClient = redis.createClient({
-    port: 6379, // default port for redis
-    host: 'localhost'
+    legacyMode: true,
+    socket: {
+        port: 6379, // default port for redis
+        host: 'localhost'
+    }
 });
 
+redisClient.connect().catch(console.error);
 
 // express middleware - any request that comes to the server goes through the middleware functions before any response is sent to the client
 app.use(session({
@@ -29,3 +33,42 @@ app.use(session({
         maxAge: 1000 * 60 * 30 // session max age in milliseconds
     }
 }));
+
+
+// create an unprotected login endpoint
+app.post('/login', (req, res) => {
+    const {email, password} = req;
+
+    //check if the credentials are correct
+    // ...
+
+    // assume that the credentials are correct
+    req.session.clientId = 'abc123';
+    req.session.myNum = 5;
+
+    res.json('you are now logged in');
+
+});
+
+// plug in another middleware that will check if the user is authenticated or not
+// all requests that are plugged in after this middleware will only be accesible if the user is logged in
+app.use((req, res, next) => {
+    if(!req.session || req.session.clientId){
+        const err = new Error('You are not logged in');
+        err.statusCode = 401;
+        next(err);
+    }
+    next();
+});
+
+// plug in all routes that the user can only access if logged in
+app.get('/profile', 
+
+    // add a middle for authorization
+    (res, req, next) => { /* check if the user has suffiecient priviliges*/ next();},
+
+    (req, res) => {
+    res.json(req.session);
+});
+
+app.listen(8080, () => console.log('server is running on port 8080'));
