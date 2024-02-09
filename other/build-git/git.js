ec225c1d7064c4045fd9007dce3ca5519eaf56f8
Julien Mihai <mihaijulien@gmail.com>
2024-02-09 17:23:39 +0200

    keep track of commits & add git log

--- other/build-git/git.js
+++ other/build-git/git.js
@@ -1,6 +1,7 @@
 function Git(name){
     this.name = name; // Repo name
     this.lastCommitId = -1; // Keep track of last commit id.
+    this.HEAD = null; // Reference to last Commit.
 }
 
 // var repo = new Git("my-repo");
@@ -10,11 +11,14 @@ function Git(name){
 function Commit(id, message) {
   this.id = id;
   this.message = message;
-  // Assume that 'this' has a 'change' property too.
+  
 }
 
 Git.prototype.commit = function(message) {
   var commit = new Commit(++this.lastCommitId, message);
+  return commit;
+  this.HEAD = commit;
+
   return commit;
 };
 
@@ -22,3 +26,27 @@ Git.prototype.commit = function(message) {
 
 // Actual command:
 // > git commit -m "Make commit work"
+
+
+Git.prototype.log = function() {
+    var history = []; // array of commits in reverse order.
+  
+    // 1. Start from last commit
+    // 2. Go back tracing to the first commit
+    // 3. Push in `history`
+    // Start from HEAD
+    var commit = this.HEAD,
+    history = [];
+
+    while (commit) {
+        history.push(commit);
+        // Keep following the parent
+        commit = commit.parent;
+    }
+  
+    return history;
+  };
+
+// Can be used as repo.log();
+// Actual command:
+// > git log
