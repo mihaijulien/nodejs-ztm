ad4b358bf1282340a7d51cb79ef46ee6730f7968
Julien Mihai <mihaijulien@gmail.com>
2024-02-10 14:14:51 +0200

    add checkout

--- other/build-git/git.js
+++ other/build-git/git.js
@@ -1,7 +1,9 @@
 function Git(name){
     this.name = name; // Repo name
     this.lastCommitId = -1; // Keep track of last commit id.
+    this.branches = []; // List of all branches.
     var master = new Branch("master", null); // null is passed as we don't have any commit yet.
+    this.branches.push(master); // Store master branch.
     this.HEAD = master; // HEAD points to current branch.
 }
 
@@ -33,6 +35,29 @@ function Branch(name, commit) {
   this.commit = commit;
 }
 
+Git.prototype.checkout = function(branchName) {
+  // Check if a branch already exists with name = branchName
+  for(var i = this.branches.length; i--; ){
+    if(this.branches[i].name === branchName){
+      console.log("Switched to existing branch: " + branchName);
+      this.HEAD = this.branches[i];
+      return this;
+    }
+  }
+  // If branch was not found, create a new one.
+  var newBranch = new Branch(branchName, this.HEAD.commit);
+  // Store branch.
+  this.branches.push(newBranch);
+  // Update HEAD
+  this.HEAD = newBranch;
+
+  console.log("Switched to new branch: " + branchName);
+  return this;
+};
+
+// Actual command:
+// > git checkout existing-branch
+// > git checkout -b new-branch
 
 Git.prototype.log = function() {
     var history = []; // array of commits in reverse order.
